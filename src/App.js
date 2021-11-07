b1f5c3cfcc8934d63585a8156000a97a569d9020
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-11-07 23:53:01 +0100

    added initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from "react-query";
+import axios from "./axios";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [currentUserId, setCurrentUserId] = useState("6155f408dbfb75b08c2106f1");
+
+	const { isLoading, error, data, refetch } = useQuery(
+		// query key: an array with a name and a variable used in the endpoint
+		["client bookings", currentUserId],
+		// note that we are importing an axios instance with base URL so we only need to change the endpoint
+		() => axios.get(`/user/bookings/${currentUserId}`),
+		{
+			enabled: true, // this stops the query from running automatically
+		}
+	);
+
+	return (
+		<div className="App">
+			<h1>Pildoras</h1>
+		</div>
+	);
 }
 
 export default App;
