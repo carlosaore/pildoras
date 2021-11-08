00487ea7749aa006ce35d3c9bea38cbe4e3e98ff
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-11-08 01:08:24 +0100

    Added code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useQuery } from "react-query";
 import axios from "./axios";
 import {useState} from "react";
+import Pildora from "./Pildora";
+
+
+
 
 const App = () => {
 	const [currentUserId, setCurrentUserId] = useState("6155f408dbfb75b08c2106f1");
 
-	const { isLoading, error, data, refetch } = useQuery(
+	const { isLoading, error, data} = useQuery(
 		// query key: an array with a name and a variable used in the endpoint
 		["client bookings", currentUserId],
 		// note that we are importing an axios instance with base URL so we only need to change the endpoint
@@ -18,6 +22,14 @@ const App = () => {
 	return (
 		<div className="App">
 			<h1>Pildoras</h1>
+			{isLoading
+				? <p>Loading...</p>
+				: error
+					? <p>Error: {error.message}</p>
+					: data.data.map(booking => (
+						<Pildora key={booking._id} {...booking} />
+                    ))
+			}
 		</div>
 	);
 }
---
+++ src/Pildora.js
@@ -0,0 +1,39 @@
+import { useQueries } from "react-query";
+import axios from "./axios";
+
+const Pildora = (props) => {
+
+	// Beware: make sure that the queryKeys match other you already
+	// have in the app to allow React Query to work share the same cache instead of
+	// querying the server for the same data again.
+	const data = useQueries([
+		{ queryKey: ['center', props.center], queryFn: () => axios.get(`center/id/${props.center}`), retry: false },
+		{ queryKey: ['employee', props.employee], queryFn: () => axios.get(`/employee/${props.employee}`), retry: false },
+		{ queryKey: ['center bookings', props.center], queryFn: () => axios.get(`center/services/${props.center}`), retry: false },
+	])
+
+	console.log(props);
+
+	console.log("center info", data[0].data?.data);
+
+	console.log("employee info", data[1].data?.data);
+
+	// props.service is an id and we don't have an endpoint to get the service with that, so we fetch all the services
+	// and then we filter the one that matches the id
+	// The result is still an array of one element so we need to get the first element
+	console.log("services info", props.service, data[2].data?.data.filter(booking => booking._id === props.service)[0]) ;
+
+	// Writing data.data.data etc. may be annoying, but try to refrain from re-saving that in a state just
+	// to have a shorter name. It will make the app slower and more error prone because it will need to recalculate
+	// on every render and recreate the state
+	return (
+		<section className="pildora">
+			<p>{`Dia: ${new Date(props.startTime)}`}</p>
+			<p>{`Center: ${data[0].data?.data?.name}`}</p>
+			<p>{`Employee: ${data[1].data?.data || "Error: This employee id was not found in the DB"}`}</p>
+			<p>{`Service: ${data[2].data?.data.filter(booking => booking._id === props.service)[0].name}`}</p>
+		</section>
+	)
+};
+
+export default Pildora;
